9509f91a6701acedcf0d608ef60dca5ee0a23479
mauricebarnum <maurice.barnum@gmail.com>
2025-05-23 13:19:23 -0700

    build common crate to include proto support

---
+++ crates/common/src/lib.rs
@@ -0,0 +1,10 @@
+#![allow(clippy::derive_partial_eq_without_eq)]
+
+pub mod proto {
+    tonic::include_proto!("io.streamnative.oxia.proto");
+    pub use oxia_client_client::OxiaClientClient;
+}
+
+fn foo() {
+    let x = proto::ShardAssignment::default();
+}
