ff05fa124f8c424c277560a63465de13da7f4230
mauricebarnum <maurice.barnum@gmail.com>
2025-06-25 15:35:59 -0700

    enable parallel requests in basic test

--- crates/client/tests/basic.rs
+++ crates/client/tests/basic.rs
@@ -13,8 +13,8 @@ async fn test_basic() -> Result<(), Box<dyn std::error::Error>> {
     //     .session_timeout(Duration::from_millis(2001))
     let server = common::TestServer::start()?;
     let builder = config::Builder::new()
-        // .max_parallel_requests(3)
         .retry(config::RetryConfig::new(3, Duration::from_millis(23)))
+        .max_parallel_requests(3)
         .session_timeout(Duration::from_millis(2001));
     let client = server.connect(Some(builder)).await?;
 
