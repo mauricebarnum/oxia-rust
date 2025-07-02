473f46194c0043956d6fc13a8e50d5c0fa576f05
mauricebarnum <maurice.barnum@gmail.com>
2025-07-02 14:17:23 -0700

    tweak proto codegen

--- crates/common/build.rs
+++ crates/common/build.rs
@@ -10,12 +10,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         // Use `Bytes` for binary fields
         .bytes(["."])
+        // Protoc optimizations
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .protoc_arg("--cpp_opt=speed")
+        .build_client(true)
+        .build_server(false) // maybe? are going to implement a mock server?
+        .emit_rerun_if_changed(true)
+        .compile_well_known_types(false)
         .compile_protos(&proto_files, &[proto_dir])?;
 
     // Recompile if proto files change
     proto_files
         .iter()
-        .for_each(|f| println!("cargo:rerun-if-changed={}", f));
+        .for_each(|f| println!("cargo:rerun-if-changed={f}"));
+
+    println!("cargo:rerun-if-changed={proto_dir}");
+    println!("cargo:rerun-if-changed=Cargo.toml");
 
     Ok(())
 }
