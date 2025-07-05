89dc2c01939877f4b11c0975cbef2428bf813ee9
mauricebarnum <maurice.barnum@gmail.com>
2025-07-05 12:00:21 -0700

    clippy cleanup

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -1,9 +1,4 @@
-#![allow(clippy::derive_partial_eq_without_eq)]
-
 pub mod proto {
     tonic::include_proto!("io.streamnative.oxia.proto");
     pub use oxia_client_client::OxiaClientClient;
 }
-
-
-
