1db7d571f726d4a0f65affcde39581645e3eec5f
mauricebarnum <maurice.barnum@gmail.com>
2025-07-11 14:16:39 -0700

    ci: check for oxia before calling go to rebuild it

--- crates/oxia-bin-util/build.rs
+++ crates/oxia-bin-util/build.rs
@@ -21,31 +21,35 @@ fn get_target_dir() -> Result<PathBuf, Box<dyn std::error::Error>> {
 fn build_oxia_cli() {
     const OXIA_MOD: &str = "github.com/oxia-db/oxia";
 
+    let target_dir = get_target_dir().unwrap();
+    let oxia_path = target_dir.join("oxia-bin");
+    let build_oxia = !oxia_path.exists();
+
     // Let's convert all paths into owned strings up-front: panic early
+    let oxia_path = oxia_path.to_str().unwrap().to_string();
     let tools_src_dir = Path::new("go");
     let go_mod = tools_src_dir.join("go.mod").to_str().unwrap().to_string();
     let go_sum = tools_src_dir.join("go.sum").to_str().unwrap().to_string();
 
-    let target_dir = get_target_dir().unwrap();
-    let oxia_path = target_dir.join("oxia-bin").to_str().unwrap().to_string();
-
-    let start = Instant::now();
-    let status = Command::new("go")
-        .current_dir(tools_src_dir)
-        .env("GOPROXY", "off")
-        .arg("build")
-        .arg("-v")
-        .arg("-mod=vendor")
-        .arg("-o")
-        .arg(&oxia_path)
-        .arg(format!("./vendor/{OXIA_MOD}/cmd"))
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .status()
-        .expect("failed to run `go build` for oxia CLI");
-    let install_time = start.elapsed();
-    assert!(status.success(), "oxia CLI installation failed");
-    println!("cargo:warning=oxia CLI build finished in {install_time:.2?} seconds");
+    if build_oxia {
+        let start = Instant::now();
+        let status = Command::new("go")
+            .current_dir(tools_src_dir)
+            .env("GOPROXY", "off")
+            .arg("build")
+            .arg("-v")
+            .arg("-mod=vendor")
+            .arg("-o")
+            .arg(&oxia_path)
+            .arg(format!("./vendor/{OXIA_MOD}/cmd"))
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .status()
+            .expect("failed to run `go build` for oxia CLI");
+        let install_time = start.elapsed();
+        println!("cargo:warning=oxia build finished in {install_time:.2?} seconds");
+        assert!(status.success(), "oxia build failed");
+    }
     println!("cargo:rerun-if-changed={go_mod}");
     println!("cargo:rerun-if-changed={go_sum}");
     println!("cargo:rustc-env=OXIA_BIN_PATH={oxia_path}");
